86a0424e182f0cc12f46df712388ece7f86b43c0
Henry Tran <henrytr4n@gmail.com>
2022-08-19 19:24:24 -0700

    add project

---
+++ pizzas/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+class Pizza(models.Model):
+    """A pizza with a name"""
+
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.name
+
+
+class Topping(models.Model):
+    """Pizza toppings"""
+
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.name
